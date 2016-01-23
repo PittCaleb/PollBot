804eabbdbec7577891477a5ed99f22563c52cec7
PittCaleb <Caleb@Hail2Pitt.org>
2016-01-23 14:30:06 -0500

    Command Line options added / required.  Fully generic code base

--- polldaddy_vote.py
+++ polldaddy_vote.py
@@ -1,4 +1,4 @@
-import requests, re, json, time
+import argparse, optparse, requests, re, json, time
 requests.packages.urllib3.disable_warnings()
 
 url = 'https://polldaddy.com/poll/'
@@ -14,21 +14,34 @@ def vote_once(form, value):
 	send = c.get(request, headers=redirect, verify=False)
 	return ('revoted' in send.url)
 
-def vote(form, value, times):
+def vote(form, value, times, timeout):
 	global redirect
 	redirect = {"Referer": url + str(form) + "/", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36", "Upgrade-Insecure-Requests":"1", "Accept-Encoding": "gzip, deflate, sdch", "Accept-Language": "en-US,en;q=0.8"}
 	for i in range(1,times+1):
 		b = vote_once(form, value)
 		if not b:
-			print ("              Voted (time number " + str(i) + ")!")
-			time.sleep(20)
+			print ("Voted (time number " + str(i) + ")!")
+			time.sleep(timeout)
 		else:
-			print ("              Locked.  Sleeping for 60 seconds.")
+			print ("Locked.  Sleeping for 60 seconds.")
 			i-=1
 			time.sleep(60)
 
-poll_id = 9277348
-answer_id = 42299039
-number_of_votes = 10
+# poll_id = 9273313 #answer_id = 42275895 #number_of_votes = 10
 
-vote(poll_id, answer_id, number_of_votes)
+# parser = optparse.OptionParser()
+# print(parser.parse_args())
+
+parser = argparse.ArgumentParser()
+parser.add_argument("pollid")
+parser.add_argument("answerid")
+parser.add_argument("numvotes")
+parser.add_argument("delay")
+args = parser.parse_args()
+poll_id = args.pollid
+answer_id = args.answerid
+num_votes = int(args.numvotes)
+delay = int(args.delay)
+
+vote(poll_id, answer_id, num_votes, delay)
+
--- vote_when_necessary.php
+++ vote_when_necessary.php
@@ -2,21 +2,27 @@
 
 $behind = 999;
 
+function showhelp() {
+	echo "PollBot Usage:\n";
+	echo "  PollBot  PollID  UniqueString\n";
+	echo "\nExample:\n";
+	echo "  PollBot  9277348  Cohen\n";
+}
+
+function FindID ($base, $poll, $searchname) {
+	$preurl = $base.$poll;
+	$predata = file_get_contents($preurl);
+	$mystart = strpos($predata, $searchname);
+	$idstart = strrpos(substr($predata,0,$mystart) , "value=");
+	return substr($predata,$idstart+7,8);
+}
+
 function getrank ($base, $poll, $myguy, $myname) {
 
 	$preurl = $base.$poll."/?view=results";
 	$predata = file_get_contents($preurl);
-	
-	// echo $predata;
-	
 	$labelstart = 'class="results"';
-	
-	//echo "\n|$labelstart|\n";
-	
 	$resultstart = strpos($predata, $labelstart);
-	
-	//echo "start=$resultstart \nresults start at \n".substr($predata, $resultstart,200);
-	
 	
 	$continue = 1;
 	$entries = 1;
@@ -28,34 +34,26 @@ function getrank ($base, $poll, $myguy, $myname) {
 			$namestart = strpos($predata, ">",$labelstart);
 			$nameend = strpos($predata, "</div>",$labelstart);
 			$names[$entries] = substr($predata, $namestart+1, $nameend-$namestart-1);
-			
 			$labelstart = strpos($predata, "class=\"votes\"",$nameend);
 			$votesstart = strpos($predata, ">",$labelstart) + 1;
 			$votesend = strpos($predata, " ",$votesstart);
 			$votes[$entries] = str_replace(",","",substr($predata, $votesstart, $votesend-$votesstart));
-			
 			$offset = $votesend;
-			
-			//echo "Entry #$entries  Name=$names[$entries]  Votes=$votes[$entries]\n";
-			
 			$entries++;
 		}
 		else {
 			$continue = 0;
 		}
 	}
+
 	$entries--;
 	$ar1 = array(10, 100, 100, 0);
 	$ar2 = array(1, 3, 2, 4);
 	array_multisort($votes, $names);
-	
-	// print_r($names);
-	// print_r($votes);
-	
+		
 	$myindex = 0;
 	for ($i = 0; $i < $entries; $i++) {
-		// echo "in loop, i = $i, entries = $entries\n";
-		if ($names[$i] == $myname) {
+		if (strpos($names[$i],$myname)) {
 			$myindex = $i;
 		}
 	}
@@ -67,31 +65,47 @@ function getrank ($base, $poll, $myguy, $myname) {
 	return $diff;
 }
 
-	set_time_limit(0);
-	$pollid = "9277348";
-	$voting_id = "42299039";
-	$myname = "Josh Cohen, Scotch Plains-Fanwood";
-	$baseurl = "https://polldaddy.com/poll/";
+//print_r($argv); echo sizeof($argv); echo "\n\n\n";  
+	
+if (sizeof($argv) != 3) {
+	echo "Incorrect number of arguments passed\n\n";
+	showhelp();
+	exit();
+}
 
-	while(true){
-		$votesneeded = getrank($baseurl, $pollid, $voting_id, $myname);
-		//echo "person behind = ".$behind."\n";
-		if ($votesneeded > 10) {
-			echo "              Keeping up with first:    Voting 10 times, please wait...\n";
-			$output = shell_exec('py C:\Temp\polldaddy\vote_josh_10.py');
-			//$output = shell_exec('dir');
-			echo $output;
-		}
-		elseif ($behind < 100) {
-			echo "              Staying away from behind: Voting 10 times, please wait...\n";
-			$output = shell_exec('py C:\Temp\polldaddy\vote_josh_10.py');
-			//$output = shell_exec('dir');
-			echo $output;
-		} else {
-			echo "              Sleeping 10 minutes...\n";
-			sleep(600);
-		}
-		sleep(3);
-		echo "Checking standing again...\n";
+if ((sizeof($argv) == 1) || ((isset($argv[1])) && ($argv[1] == "help"))) {
+	showhelp();
+	exit();
+}
+
+$pollid = $argv[1];
+$myname = $argv[2];
+$baseurl = "https://polldaddy.com/poll/";
+$votetimes = 10;
+$timeout = 20;
+
+$voting_id = FindID($baseurl, $pollid, $myname);
+
+$votescript = 'py polldaddy_vote.py '.$pollid.' '.$voting_id.' '.$votetimes.' '.$timeout;
+
+set_time_limit(0);
+
+while(true){
+	$votesneeded = getrank($baseurl, $pollid, $voting_id, $myname);
+	if ($votesneeded > 10) {
+		echo "              Keeping up with first:    Voting 10 times, please wait...\n";
+		$output = shell_exec($votescript);
+		echo $output;
 	}
+	elseif ($behind < 100) {
+		echo "              Staying away from behind: Voting 10 times, please wait...\n";
+		$output = shell_exec($votescript);
+		echo $output;
+	} else {
+		echo "              Sleeping 10 minutes...\n";
+		sleep(600);
+	}
+	sleep(3);
+	echo "Checking standing again...\n";
+}
 ?>
